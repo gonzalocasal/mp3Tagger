be5f1c8556b26c04bde6e2b1f3ca8ec12c49379e
Gonzalo <gonzalocasal@gmail.com>
2019-01-12 18:10:58 -0300

    version 1.0

--- src/main/java/com/tagmp3/Application.java
+++ src/main/java/com/tagmp3/Application.java
@@ -1,71 +1,49 @@
 package com.tagmp3;
 
 import com.mpatric.mp3agic.*;
-import com.tagmp3.file.FileParser;
-import com.tagmp3.web.GoogleSearcher;
-import org.apache.logging.log4j.LogManager;
-import org.apache.logging.log4j.Logger;
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.boot.CommandLineRunner;
-import org.springframework.boot.SpringApplication;
-import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.context.ApplicationContext;
-import org.springframework.context.annotation.Bean;
-
 import java.io.File;
 import java.io.IOException;
+import java.util.logging.Logger;
 
-@SpringBootApplication
 public class Application {
 
-    private static final Logger logger = LogManager.getLogger(Application.class);
+    private final static Logger LOGGER = Logger.getLogger("Application");
 
-    @Autowired
-    FileParser fileParser;
-
-    @Autowired
-    GoogleSearcher googleSearcher;
-
-    public static void main(String[] args) {
-        SpringApplication.run(Application.class, args);
+    public static void main(String[] args) throws UnsupportedTagException, NotSupportedException, InvalidDataException, IOException {
+        File dir = new File(".");
+        File out = new File("result/");
+        out.mkdir();
+        File[] originalFiles = dir.listFiles();
+        readFolder(originalFiles);
+        LOGGER.info("TASK COMPLETED");
     }
-
-    @Bean
-    public CommandLineRunner commandLineRunner(ApplicationContext ctx) {
-        return args -> {
-
-            File dir = new File("./music");
-            File[] filesList = dir.listFiles();
-            if (filesList !=null ) {
-                 for (File file : filesList) {
-                     String songName = fileParser.parseSongName(file.getName());
-                     //String songYear = googleSearcher.findSongYear(songName);
-                     //tagYear(file, songYear);
-                     String songAlbum = googleSearcher.findSongAlbum(songName);
-                     tagYear(file, "", songAlbum);
-                 }
-                 logger.info("TASK COMPLETED");
-             }else{
-                logger.info("Files not found");
+    private static void readFolder(File[] originalFiles) throws InvalidDataException, IOException, UnsupportedTagException, NotSupportedException {
+        for (File file : originalFiles) {
+            if (file.getName().contains(".mp3")) {
+                Mp3File mp3 = new Mp3File(file.getAbsolutePath());
+                ID3v2 tag = mp3.getId3v2Tag();
+                LOGGER.info(file.getName());
+                appy(mp3, tag, file.getName());
             }
-        };
+        }
     }
 
-    private void tagYear(File file, String songYear, String songAlbum) throws IOException, UnsupportedTagException, InvalidDataException, NotSupportedException {
-        Mp3File mp3file = new Mp3File(file.getAbsolutePath());
-        ID3v1 id3v1Tag  =  mp3file.getId3v1Tag();
-        ID3v2 id3v2Tag = mp3file.getId3v2Tag();
-        if (!songYear.isEmpty()) {
-            id3v1Tag.setYear(songYear);
-            id3v2Tag.setYear(songYear);
-        }
-        if (!songAlbum.isEmpty()) {
-            id3v1Tag.setAlbum(songAlbum);
-            id3v2Tag.setAlbum(songAlbum);
-        }
-        mp3file.setId3v1Tag(id3v1Tag);
-        mp3file.setId3v2Tag(id3v2Tag);
-        mp3file.save(file.getName());
-    }
 
+    private static void appy(Mp3File mp3, ID3v2 oldTag, String fileName) throws IOException, NotSupportedException {
+        String songName = fileName.replace(".mp3", "").trim();
+        String artist = songName.split("-")[0];
+        String title = songName.split("-")[1];
+        String year = oldTag.getYear();
+        byte[] albumImage = oldTag.getAlbumImage();
+        String mimeType = oldTag.getAlbumImageMimeType();
+        ID3v23Tag newTag = new ID3v23Tag();
+        newTag.setArtist(artist);
+        newTag.setTitle(title);
+        newTag.setAlbum(title);
+        newTag.setYear(year);
+        newTag.setAlbumImage(albumImage,mimeType);
+        mp3.removeId3v1Tag();
+        mp3.setId3v2Tag(newTag);
+        mp3.save("result/"+fileName);
+    }
 }
