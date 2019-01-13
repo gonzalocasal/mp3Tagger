859e37a3e6347012cb0d370f04ea275113b96579
Gonzalo <gonzalocasal@gmail.com>
2019-01-13 14:26:03 -0300

    fix white space on album tag

--- src/main/java/com/tagmp3/Application.java
+++ src/main/java/com/tagmp3/Application.java
@@ -7,19 +7,22 @@ import java.util.logging.Logger;
 
 public class Application {
 
-    private final static Logger LOGGER = Logger.getLogger("Application");
+    private static final Logger LOGGER = Logger.getLogger("Application");
+    private static final String OUTPUT_PATH = "result/";
+    private static final String MP3_FILE_EXTENSION = ".mp3";
 
     public static void main(String[] args) throws UnsupportedTagException, NotSupportedException, InvalidDataException, IOException {
         File dir = new File(".");
-        File out = new File("result/");
+        File out = new File(OUTPUT_PATH);
         out.mkdir();
         File[] originalFiles = dir.listFiles();
         readFolder(originalFiles);
         LOGGER.info("TASK COMPLETED");
     }
+
     private static void readFolder(File[] originalFiles) throws InvalidDataException, IOException, UnsupportedTagException, NotSupportedException {
         for (File file : originalFiles) {
-            if (file.getName().contains(".mp3")) {
+            if (file.getName().contains(MP3_FILE_EXTENSION)) {
                 Mp3File mp3 = new Mp3File(file.getAbsolutePath());
                 ID3v2 tag = mp3.getId3v2Tag();
                 LOGGER.info(file.getName());
@@ -28,9 +31,8 @@ public class Application {
         }
     }
 
-
     private static void appy(Mp3File mp3, ID3v2 oldTag, String fileName) throws IOException, NotSupportedException {
-        String songName = fileName.replace(".mp3", "");
+        String songName = fileName.replace(MP3_FILE_EXTENSION, "");
         String artist = songName.split("-")[0].trim();
         String title = songName.split("-")[1].trim();
         String year = oldTag.getYear();
@@ -44,6 +46,6 @@ public class Application {
         newTag.setAlbumImage(albumImage,mimeType);
         mp3.removeId3v1Tag();
         mp3.setId3v2Tag(newTag);
-        mp3.save("result/"+fileName);
+        mp3.save(OUTPUT_PATH+fileName);
     }
 }
