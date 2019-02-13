80f94fa2bf57b7eb66b14ed8cff9ae1c00e039cd
gonzalo <gonzalocasal@gmail.com>
2019-02-12 23:13:21 -0300

    get year from google search

--- src/main/java/com/tagmp3/Application.java
+++ src/main/java/com/tagmp3/Application.java
@@ -1,13 +1,15 @@
 package com.tagmp3;
 
+import com.tagmp3.com.tagmp3.service.YearTagResolver;
 import org.jaudiotagger.audio.AudioFile;
 import org.jaudiotagger.audio.AudioFileIO;
 import org.jaudiotagger.tag.FieldKey;
 import org.jaudiotagger.tag.Tag;
-import org.jaudiotagger.tag.datatype.Artwork;
 import org.jaudiotagger.tag.id3.ID3v23Tag;
+
 import java.io.File;
 import java.util.Objects;
+import java.util.logging.Level;
 import java.util.logging.Logger;
 
 public class Application {
@@ -16,6 +18,7 @@ public class Application {
     private static final String MP3_FILE_EXTENSION = ".mp3";
 
     public static void main(String[] args)  {
+        Logger.getLogger("org.jaudiotagger").setLevel(Level.OFF);
         long time1 = System.nanoTime();
         File dir = new File(".");
         File[] originalFiles = dir.listFiles();
@@ -39,26 +42,22 @@ public class Application {
     private static void processFile(File file) throws Exception {
         AudioFile mp3 = AudioFileIO.read(file);
         Tag oldTag = mp3.getTag();
-        String songName = file.getName().replace(MP3_FILE_EXTENSION, "");
-        String artist = songName.split(" - ")[0].trim();
-        String title = songName.split(" - ")[1].trim();
-        String year = oldTag.getFirst(FieldKey.YEAR);
-        String genre = oldTag.getFirst(FieldKey.GENRE);
-        Artwork artwork = oldTag.getFirstArtwork();
-        applyChanges(mp3, songName, artist, title, year, genre, artwork);
-    }
-
-    private static void applyChanges(AudioFile mp3, String songName, String artist, String title, String year, String genre, Artwork artwork) throws Exception  {
-        AudioFileIO.delete(mp3);
         ID3v23Tag newTag = new ID3v23Tag();
-        newTag.setField(FieldKey.ARTIST,artist);
-        newTag.setField(FieldKey.TITLE,title);
+
+        String songName = file.getName().replace(MP3_FILE_EXTENSION, "");
+
+        newTag.setField(FieldKey.ARTIST,songName.split(" - ")[0].trim());
+        newTag.setField(FieldKey.TITLE,songName.split(" - ")[1].trim());
         newTag.setField(FieldKey.ALBUM,songName);
-        newTag.setField(FieldKey.YEAR,year);
-        newTag.setField(FieldKey.GENRE,genre);
-        newTag.setField(artwork);
+        newTag.setField(FieldKey.YEAR,YearTagResolver.getYear(songName));
+        newTag.setField(FieldKey.GENRE,oldTag.getFirst(FieldKey.GENRE));
+        newTag.setField(oldTag.getFirstArtwork());
         newTag.deleteField(FieldKey.TRACK);
+
+        AudioFileIO.delete(mp3);
+
         mp3.setTag(newTag);
         AudioFileIO.write(mp3);
     }
+
 }
