a39f40d1ca43be4c7ec1f284aa6656b7c2fbc98b
Gonzalo Casal <gonzalo.casal@directa24.com>
2022-12-19 07:30:56 -0300

    File creation date in comment tag.

--- src/main/java/com/tagmp3/service/FileTagService.java
+++ src/main/java/com/tagmp3/service/FileTagService.java
@@ -5,7 +5,6 @@ import org.apache.logging.log4j.Logger;
 import org.jaudiotagger.audio.AudioFile;
 import org.jaudiotagger.audio.AudioFileIO;
 import org.jaudiotagger.audio.mp3.MP3File;
-import org.jaudiotagger.tag.FieldDataInvalidException;
 import org.jaudiotagger.tag.FieldKey;
 import org.jaudiotagger.tag.Tag;
 import org.jaudiotagger.tag.id3.AbstractID3v2Tag;
@@ -14,20 +13,22 @@ import org.jsoup.Jsoup;
 import org.jsoup.nodes.Document;
 
 import java.io.File;
+import java.io.IOException;
 import java.net.URLEncoder;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.attribute.BasicFileAttributes;
+import java.text.SimpleDateFormat;
+import java.util.Date;
 import java.util.Optional;
 
-import static com.tagmp3.util.Constants.FILENAME_ARTIST_TRACK_SEPARATOR;
-import static com.tagmp3.util.Constants.GOOGLE_SEARCH_ENCODE;
-import static com.tagmp3.util.Constants.GOOGLE_SEARCH_MAGIC_CLASS;
-import static com.tagmp3.util.Constants.GOOGLE_SEARCH_URL;
-import static com.tagmp3.util.Constants.GOOGLE_SEARCH_URL_SEARCH_PARAM;
-import static com.tagmp3.util.Constants.MP3_FILE_EXTENSION;
-import static com.tagmp3.util.Constants.MP3_PROCESSED_COMMENT;
+import static com.tagmp3.util.Constants.*;
 
 public class FileTagService {
 
-    static final Logger log = LogManager.getLogger(FileTagService.class);
+    private static final Logger log = LogManager.getLogger(FileTagService.class);
+
+    private final SimpleDateFormat formatter = new SimpleDateFormat("yyyy-MM-dd");
 
     public void processFile(File file, String genre) {
         if (!file.getName().contains(MP3_FILE_EXTENSION)) {
@@ -37,7 +38,7 @@ public class FileTagService {
             AudioFile mp3 = AudioFileIO.read(file);
             AbstractID3v2Tag oldTag = ((MP3File) mp3).getID3v2Tag();
 
-            if (MP3_PROCESSED_COMMENT.equals(oldTag.getFirst(FieldKey.COMMENT))){
+            if (MP3_PROCESSED_COMMENT.equals(oldTag.getFirst(FieldKey.ENGINEER))){
                 return;
             }
             log.info("Processing: {}", file);
@@ -52,7 +53,7 @@ public class FileTagService {
         }
     }
 
-    private ID3v23Tag generateNewTag(File file, String genre, Tag oldTag) throws FieldDataInvalidException {
+    private ID3v23Tag generateNewTag(File file, String genre, Tag oldTag) throws Exception {
         String fileName = file.getName().replace(MP3_FILE_EXTENSION, "");
         ID3v23Tag newTag = new ID3v23Tag();
         try {
@@ -60,14 +61,21 @@ public class FileTagService {
             newTag.setField(FieldKey.TITLE, fileName.split(FILENAME_ARTIST_TRACK_SEPARATOR)[1].trim());
             newTag.setField(FieldKey.ALBUM, fileName.split(FILENAME_ARTIST_TRACK_SEPARATOR)[0].trim());
             newTag.setField(FieldKey.GENRE, genre);
-            newTag.setField(FieldKey.COMMENT, MP3_PROCESSED_COMMENT);
+            newTag.setField(FieldKey.ENGINEER, MP3_PROCESSED_COMMENT);
             newTag.setField(FieldKey.YEAR, getYearFromGoogle(fileName).orElse(oldTag.getFirst(FieldKey.YEAR)));
+            newTag.setField(FieldKey.COMMENT, getCreationDate(file));
             newTag.setField(oldTag.getFirstArtwork());
         } catch (Exception e) {
             log.error("ERROR occurred trying to generate new tags for: {}", file.getName());
             throw e;
         }
         return newTag;
+    }
+
+    private String getCreationDate(File file) throws IOException {
+        BasicFileAttributes attr = Files.readAttributes(Path.of(file.getPath()), BasicFileAttributes.class);
+        Date creationDate = new Date(attr.creationTime().toMillis());
+        return formatter.format(creationDate);
     }
 
     private Optional<String> getYearFromGoogle(String songName) {
